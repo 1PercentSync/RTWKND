0f06c7c804a8cc9d444fc7dda396ca3b1e6d8670
1PercentSync <127626920+1PercentSync@users.noreply.github.com>
2025-12-21 19:56:59 -0700

    Refactor variable declarations in main.cpp
    Changed several variable declarations to use const or constexpr for improved clarity and immutability. Also updated type casting for viewport_width calculation and function parameter for hit_sphere to use 'const double'.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "ray.h"
 #include "vec3.h"
 
-bool hit_sphere(const point3 &center, double radius, const ray &r) {
+bool hit_sphere(const point3 &center, const double radius, const ray &r) {
     const vec3 oc = center - r.origin();
     const auto a = dot(r.direction(), r.direction());
     const auto b = -2.0 * dot(r.direction(), oc);
@@ -34,23 +34,23 @@ int main() {
     image_height = (image_height < 1) ? 1 : image_height;
 
     // Camera
-    auto focal_length = 1.0;
+    constexpr auto focal_length = 1.0;
     constexpr auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width) / image_height);
-    auto camera_center = point3(0, 0, 0);
+    const auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
+    const auto camera_center = point3(0, 0, 0);
 
     // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
+    const auto viewport_u = vec3(viewport_width, 0, 0);
+    const auto viewport_v = vec3(0, -viewport_height, 0);
 
     // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    auto pixel_delta_u = viewport_u / image_width;
-    auto pixel_delta_v = viewport_v / image_height;
+    const auto pixel_delta_u = viewport_u / image_width;
+    const auto pixel_delta_v = viewport_v / image_height;
 
     // Calculate the location of the upper left pixel.
-    auto viewport_upper_left = camera_center
+    const auto viewport_upper_left = camera_center
                                - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+    const auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
 
     // Write to the project root
