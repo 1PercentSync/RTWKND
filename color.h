e3e76bcdbc5df43549d3b62e539678d29a82b650
1PercentSync <127626920+1PercentSync@users.noreply.github.com>
2025-12-21 17:27:48 -0700

    Add color utility header and update CMakeLists
    Introduces color.h for color handling and pixel output, and updates CMakeLists.txt to include color.h in the build. This improves code organization by separating color logic from other components.

---
+++ color.h
@@ -0,0 +1,24 @@
+#ifndef RTWKND_COLOR_H
+#define RTWKND_COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+using color = vec3;
+
+inline void write_color(std::ostream &out, const color &pixel_color) {
+    const auto r = pixel_color.x();
+    const auto g = pixel_color.y();
+    const auto b = pixel_color.z();
+
+    // Translate the [0,1] component values to the byte range [0,255].
+    const int rbyte = static_cast<int>(255.999 * r);
+    const int gbyte = static_cast<int>(255.999 * g);
+    const int bbyte = static_cast<int>(255.999 * b);
+
+    // Write out the pixel color components.
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif //RTWKND_COLOR_H
