59a2a417125bffb41f054bc9e451e5d774a91cfe
1PercentSync <127626920+1PercentSync@users.noreply.github.com>
2025-12-21 21:58:48 -0700

    Add interval utility class and update includes
    Introduced a new interval.h header providing an interval class for range operations. Updated CMakeLists.txt and rtweekend.h to include interval.h in the build and common headers.

---
+++ interval.h
@@ -0,0 +1,32 @@
+#ifndef RTWKND_INTERVAL_H
+#define RTWKND_INTERVAL_H
+
+class interval {
+public:
+    double min, max;
+
+    interval() : min(+infinity), max(-infinity) {
+    } // The default interval is empty
+
+    interval(const double min, const double max) : min(min), max(max) {
+    }
+
+    [[nodiscard]] double size() const {
+        return max - min;
+    }
+
+    [[nodiscard]] bool contains(const double x) const {
+        return min <= x && x <= max;
+    }
+
+    [[nodiscard]] bool surrounds(const double x) const {
+        return min < x && x < max;
+    }
+
+    static const interval empty, universe;
+};
+
+const interval interval::empty = interval(+infinity, -infinity);
+const interval interval::universe = interval(-infinity, +infinity);
+
+#endif //RTWKND_INTERVAL_H
